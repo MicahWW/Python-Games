dc393816b7ceb03b8301efd7cb57c5cbcf8545a4
Micah_Warren <26724012+MicahWW@users.noreply.github.com>
2023-02-02 16:08:28 -0600

    Fixed main

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ from TicTacToe import TicTacToe
 games = [TicTacToe()]
 
 while True:
-	print(f'There are {len(games)} games...')
+	print(f"There are {len(games)} games...")
 	for idx, game in enumerate(games, start=1):
-		print(f'{idx}: {game.gameName()}')
+		print(f"{idx}: {game.gameName()}")
 
 	while True:
-		print('\nWhich do you want to play?')
-		selection = input('To select a game enter it\'s number, or to exit enter \'exit\': ')
+		print("\nWhich do you want to play?")
+		selection = input("To select a game enter it's number, or to exit enter 'exit': ")
 
 		if selection.isnumeric():
 			selection = int(selection) - 1
@@ -18,14 +18,14 @@ while True:
 				cont = True
 				while cont:
 					games[selection].terminalGame()
-					cont = input('Would you like to play this game again? (y/n): ')
-					cont = True if cont.lower() == 'y' else False
+					cont = input("Would you like to play this game again? (y/n): ")
+					cont = True if cont.lower() == "y" else False
 				break
 			else:
-				print('There are not that many games!')
+				print("There are not that many games!")
 				continue
-		elif selection == 'exit':
+		elif selection == "exit":
 			exit(0)
 		else:
-			print('Invalid input')
+			print("Invalid input")
 			continue
